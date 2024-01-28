a842b6022fcb6951160f6f131a76c3ef8543abda
Claudio9616 <clares9611@gmail.com>
2024-01-28 17:36:11 +0100

    add todolist completed

---
+++ script/script.js
@@ -0,0 +1,39 @@
+console.log( Vue);  
+const {createApp} = Vue  
+const app = createApp ({ 
+    data: () => ({ 
+        tasks: [ 
+            { id: 1, done: false, text: 'Fare la spesa' }, 
+            { id: 2, done: true, text: 'Lavare i capelli' }, 
+            { id: 3, done: false, text: 'Comprare una marca da bollo' }, 
+            { id: 4, done: false, text: 'Aggiornare il PC' }, 
+        ], 
+        newTask: '' 
+    }), 
+    methods: { 
+        // Funzione per spuntare gli elementi dalla list 
+        toggleTask(id) { 
+            this.tasks.forEach(task => { 
+                if (id === task.id) { 
+                    task.done = !task.done 
+                } 
+            }) 
+        }, 
+        // Funzione per rimuovere un task 
+        deleteTask(id) { 
+            this.tasks = this.tasks.filter(task => id !== task.id); 
+        }, 
+        // Funzione per aggiungere un nuovo task 
+        addTask() { 
+            const newTask = {  
+                id: new Date().toISOString(), 
+                done: false, 
+                text: this.newTaskText 
+            }              
+            this.tasks.push(newTask); 
+            this.newTaskText = ''; 
+        } 
+    } 
+}); 
+ 
+app.mount('#root')
